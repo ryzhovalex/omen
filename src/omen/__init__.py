4299b0aff139aef75c9e4dceb645ffe0502d18c6
Alex <thed4rkof@gmail.com>
2021-09-30 18:21:56 +0400

    .: change constants reference

--- src/omen/__init__.py
+++ src/omen/__init__.py
@@ -1,3 +1,4 @@
+from .helpers import constants
 from .errors.error import Error
 from .ui.views.view import View
 from .helpers.helper import Helper
@@ -13,7 +14,6 @@ from .models.domains.database import Database
 from .helpers.decorators import login_required
 from .ui.controllers.controller import Controller
 from .helpers.cells import (Cell, ViewCell, ConfigCell, InjectionCell, MapperCell, TurboCell)
-from .helpers.constants import (Json, Path, HttpMethod, HTTP_METHODS, ConfigLoadType, CONFIG_LOAD_TYPES)
 
 
 __version__ = "0.1.0-proto"
