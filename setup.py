68a0951a4a1b43c67a92da7e09508ac106a4b356
Alex <thed4rkof@gmail.com>
2021-12-15 16:19:17 +0400

    v0.3.0dev10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="puft",
-    version="0.3.0.dev9",
+    version="0.3.0.dev10",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
