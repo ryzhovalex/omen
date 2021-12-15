a6973765d016cba8ced10b05d037e9113e94f52c
Alex <thed4rkof@gmail.com>
2021-12-15 18:55:04 +0400

    workon cli

--- puft/helpers/cli.py
+++ puft/helpers/cli.py
@@ -3,7 +3,7 @@ import argparse
 import subprocess
 from typing import Tuple, Literal
 
-from warepy import format_message
+from warepy import format_message, join_paths
 
 # Attempt relative import, and it its failed, use absolute import.
 try:
@@ -16,8 +16,10 @@ def main() -> int:
     args = parse_input()
 
     mode = args.mode
+    root_dir = args.root_dir
+    user_environs_file_path = args.user_environs_file_path
     host, port = parse_hostname(args.hostname)
-    python_bin = args.python_bin
+    python_bin = parse_python_bin(args.python_bin)
 
     # Generate and run flask-related command.
     action = resolve_flask_action(mode)
@@ -27,7 +29,8 @@ def main() -> int:
     if "db" not in action:
         cmd += f" -h {host} -p {port}"
 
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, text=True, env=generate_environs(args), shell=True) as process:
+    subprocess_environs = generate_environs(args).update(parse_user_environs_from_file(caller_root_dir=root_dir, user_environs_file_path=user_environs_file_path))
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, text=True, env=subprocess_environs, shell=True) as process:
         for line in process.stdout:
             print(line, end="")
     if process.returncode != 0:
@@ -70,15 +73,62 @@ def parse_hostname(hostname: str) -> Tuple[str, str]:
         return (splitted[0], splitted[1])
 
 
+def parse_python_bin(python_bin: str) -> str:
+    """Parse given python bin path and return appropriate implementation.
+    
+    Look for virtual environment environs, if `python_bin = "python3"`."""
+    if python_bin == "python3":
+        virtual_env = os.environ.get("VIRTUAL_ENV", None)
+        if virtual_env:
+            return f"{virtual_env}/bin/python3"
+        else:
+            return "python3"
+
+
+def parse_user_environs_from_file(caller_root_dir: str, user_environs_file_path: str) -> dict:
+    """Parse environs from file by given path and return mapping with them.
+    
+    If `user_environs_file_path` is None or empty, look for file `environs` in given `caller_root_dir`.
+    
+    Raise:
+        ValueError:
+            If any environ within file at target path violated format `ENVIRON_NAME=VALUE\\n`"""
+    # Resolve path.
+    if user_environs_file_path:
+        target_path = user_environs_file_path
+    else:
+        target_path = join_paths(caller_root_dir, "./environs")
+    
+    # Open target file and read all lines.
+    with open(target_path, "r") as file:
+        lines = file.readlines()
+
+    # Parse received lines.
+    values_by_environ = {}
+    for line in lines:
+        formatted_line = line.replace("\n", "").split("=")
+        if len(formatted_line) != 2:
+            raise ValueError(format_message("Wrong format of given file environ: {}.", line))
+        else:
+            values_by_environ[line[0]] = line[1]
+    return values_by_environ
+
+
 def parse_input() -> argparse.Namespace:
     """Parse cli input and return argparse.Namespace object."""
     # TODO: Add descriptions to args.
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", nargs="?", default="dev", choices=MODES)
-    parser.add_argument("source_file", nargs="?", default="main")
-    parser.add_argument("hostname", nargs="?", default="127.0.0.1:5000")
+    parser.add_argument("mode", nargs="*", default="dev", choices=MODES)
+    parser.add_argument("source_file", nargs="*", default="main")
+    parser.add_argument("hostname", nargs="*", default="127.0.0.1:5000")
     parser.add_argument("-dir", dest="root_dir", default=os.getcwd())
-    parser.add_argument("-py", dest="python_bin", default="python3")
+    parser.add_argument("-py", dest="python_bin", default="python3", help="Python bin path. If default, look also for $VIRTUAL_ENV environ.")
+    parser.add_argument(
+        "-envsfile", 
+        dest="user_environs_file_path", 
+        default=None, 
+        help="Path to file with user environs in format `ENVIRON_NAME=VALUE\\n`. Defaults to file `./environs` in given directory (calling directory by default)."
+    )
     return parser.parse_args()
 
 
