d1d5171152c9846e7248f49737a15f22f928b2ac
ryzhovalex <thed4rkof@gmail.com>
2022-02-11 17:07:20 +0400

    Update constants to typing.get_args()

--- puft/helpers/constants.py
+++ puft/helpers/constants.py
@@ -1,23 +1,23 @@
 """Module with constants and typehints."""
-from typing import Any, List, Dict, Tuple, Union, Callable, Literal
+from typing import Any, List, Dict, Tuple, Union, Callable, Literal, get_args
 
-from warepy import get_literal_args
+from puft.models.domains.puft import Puft
 
 
 PuftDatabaseMode = Literal["init", "migrate", "upgrade"]
-PUFT_DATABASE_MODES = get_literal_args(PuftDatabaseMode)
+PUFT_DATABASE_MODES = get_args(PuftDatabaseMode)
 #
 PuftRunMode = Literal["dev", "prod", "test"]
-PUFT_RUN_MODES = get_literal_args(PuftRunMode)
+PUFT_RUN_MODES = get_args(PuftRunMode)
 #
 PuftConstructMode = Literal["deploy"]
-PUFT_CONSTRUCT_MODES = get_literal_args(PuftConstructMode)
+PUFT_CONSTRUCT_MODES = get_args(PuftConstructMode)
 #
 PuftMode = Union[PuftDatabaseMode, PuftRunMode, PuftConstructMode]
 PUFT_MODES = PUFT_DATABASE_MODES + PUFT_RUN_MODES + PUFT_CONSTRUCT_MODES
 
 HttpMethod = Literal["GET", "POST", "PUT", "DELETE"]
-HTTP_METHODS = get_literal_args(HttpMethod)
+HTTP_METHODS = get_args(HttpMethod)
 
 TurboAction = Literal["append", "prepend", "replace", "update", "remove"]
-TURBO_ACTIONS = get_literal_args(TurboAction)
+TURBO_ACTIONS = get_args(TurboAction)
