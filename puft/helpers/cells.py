14026a342cf5988f7cceadab99e902087a884d20
ryzhovalex <thed4rkof@gmail.com>
2022-02-22 13:11:21 +0400

    Refactor cells hierarchy

--- puft/helpers/cells.py
+++ puft/helpers/cells.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import json
 from dataclasses import dataclass
-from typing import Any, List, Dict, Union, Callable, Literal, TYPE_CHECKING, Type
+from typing import Any, TYPE_CHECKING, Type, Sequence, TypeVar
+
+from warepy import logger, format_message, join_paths, load_yaml
 
 if TYPE_CHECKING:
     # Import at type checking with future.annotations to avoid circular imports and use just for typehints.
@@ -23,24 +26,97 @@ if TYPE_CHECKING:
 
 @dataclass
 class Cell:
-    name: str
+    pass
 
 
 @dataclass
-class ConfigCell(Cell):
+class NamedCell(Cell):
+    name: str
+
+    @staticmethod
+    @logger.catch
+    def find_by_name(cells: Sequence[NamedCell], name: str) -> NamedCell:
+        """Traverse through given list of cells and return first one with specified name.
+        
+        raise:
+            ValueError: 
+                No cell with given name found.
+        """
+
+        for cell in cells:
+            if cell.name == name:
+                return cell
+        raise ValueError(format_message("No cell with given name {} found."))
+
+    @staticmethod
+    @logger.catch
+    def generate_cells_by_name(cells: list[NamedCell]) -> dict[str, NamedCell]:
+        """Traverse through given cells names and return dict with these cells as values and their names as keys."""
+        cells_by_name = {}
+        for cell in cells:
+            cells_by_name[cell.name] = cell
+        return cells_by_name
+
+
+@dataclass
+class ConfigCell(NamedCell):
     """Config cell which can be used to load configs to appropriate instance's configuration by name."""
     source: str
 
+    @staticmethod
+    @logger.catch
+    def parse_config_cell(config_cell: ConfigCell, root_path: str, update_with: dict = None) -> dict:
+        """Parse given config cell and return configuration dictionary.
+        
+        NOTE: This function performs automatic path absolutize - all paths starting with "./" within config 
+        will be joined to given root path.
+
+        Args:
+            config_cell: Configuration cell to parse from.
+            root_path: Path to join config cell source with.
+            update_with (optional): dictionary to update config cell mapping with. Defaults to None.
+        
+        Raise:
+            ValueError: If given config cell has non-relative source path.
+            ValueError: If given config cell's source has unrecognized extension."""
+        config = {}
+        config_path = join_paths(root_path, config_cell.source)
+
+        if config_cell.source[0] != "." and config_cell.source[1] != "/":
+            error_message = format_message("Given config cell has non-relative source path {}", config_cell.source)
+            raise ValueError(error_message)
+
+        # Fetch config's extension.
+        if "json" in config_path[-5:len(config_path)]:
+            with open(config_path, "r") as config_file:
+                config = json.load(config_file)
+        elif "yaml" in config_path[-5:len(config_path)]:
+            config = load_yaml(config_path)
+        else:
+            error_message = format_message("Unrecognized config cell source's extension.")
+            raise ValueError(error_message)
+
+        # Traverse all values and find paths required to be joined to the root path.
+        for k, v in config.items():
+            if type(v) == str:
+                if v[0] == "." and v[1] == "/":
+                    config[k] = join_paths(root_path, v)
+
+        # Update given config with extra dictionary if this dictionary given and not empty.
+        if update_with:
+            config.update(update_with)
+        return config
+
 
 @dataclass
-class InjectionCell(Cell):
+class InjectionCell(NamedCell):
     """Crucial core dependency injection cell united Controller, Service and Domain object in one chain."""
     controller_class: Type[Controller]
-    controller_kwargs: Dict[str, Any]
+    controller_kwargs: dict[str, Any]
     service_class: Type[Service]
-    service_kwargs: Dict[str, Any]
+    service_kwargs: dict[str, Any]
     domain_class: Type[Domain]
-    domain_kwargs: Dict[str, Any]
+    domain_kwargs: dict[str, Any]
 
 
 @dataclass
@@ -59,20 +135,20 @@ class DatabaseInjectionCell(InjectionCell):
 
 
 @dataclass
-class MapperCell(Cell):
+class MapperCell(NamedCell):
     mapper_class: Type[Mapper]
     model: Type[native_db.Model]
-    mapper_kwargs: Dict[str, Any]
+    mapper_kwargs: dict[str, Any]
 
 
 @dataclass
-class ViewCell(Cell):
+class ViewCell(NamedCell):
     name: str  # `name` == view's final endpoint, e.g. `objective.basic`.
     view_class: Type[View]
-    view_kwargs: Dict[str, Any]
+    view_kwargs: dict[str, Any]
     route: str  # Route will be the same for all methods.
 
 
 @dataclass
-class EmitterCell(Cell):
+class EmitterCell(NamedCell):
     emitter_class: Type[Emitter]
--- puft/tools/__init__.py
+++ puft/tools/__init__.py
@@ -38,6 +38,7 @@ def generate_success_response(data: Any, is_json_enabled: bool, ok_status_code: 
 
     return response
 
+
 @logger.catch
 def generate_error_response(error: Exception, status_code: int = 400) -> Response:
     """Generate error response with given error and return this response."""
@@ -46,58 +47,6 @@ def generate_error_response(error: Exception, status_code: int = 400) -> Respons
     response = make_response(json_data)
     response.status_code = status_code
     return response
-
-
-@logger.catch
-def generate_cells_by_name(cells: List[Cell]) -> Dict[str, Cell]:
-    """Traverse through given cells names and return dict with these cells as values and their names as keys."""
-    cells_by_name = {}
-    for cell in cells:
-        cells_by_name[cell.name] = cell
-    return cells_by_name
-
-
-@logger.catch
-def parse_config_cell(config_cell: ConfigCell, root_path: str, update_with: dict = None) -> dict:
-    """Parse given config cell and return configuration dictionary.
-    
-    NOTE: This function performs automatic path absolutize - all paths starting with "./" within config will be joined to given root path.
-
-    Args:
-        config_cell: Configuration cell to parse from.
-        root_path: Path to join config cell source with.
-        update_with (optional): Dictionary to update config cell mapping with. Defaults to None.
-    
-    Raise:
-        ValueError: If given config cell has non-relative source path.
-        ValueError: If given config cell's source has unrecognized extension."""
-    config = {}
-    config_path = join_paths(root_path, config_cell.source)
-
-    if config_cell.source[0] != "." and config_cell.source[1] != "/":
-        error_message = format_message("Given config cell has non-relative source path {}", config_cell.source)
-        raise ValueError(error_message)
-
-    # Fetch config's extension.
-    if "json" in config_path[-5:len(config_path)]:
-        with open(config_path, "r") as config_file:
-            config = json.load(config_file)
-    elif "yaml" in config_path[-5:len(config_path)]:
-        config = load_yaml(config_path)
-    else:
-        error_message = format_message("Unrecognized config cell source's extension.")
-        raise ValueError(error_message)
-
-    # Traverse all values and find paths required to be joined to the root path.
-    for k, v in config.items():
-        if type(v) == str:
-            if v[0] == "." and v[1] == "/":
-                config[k] = join_paths(root_path, v)
-
-    # Update given config with extra dictionary if this dictionary given and not empty.
-    if update_with:
-        config.update(update_with)
-    return config
 
 
 @logger.catch
@@ -144,12 +93,3 @@ def do_or_flash(func: Callable, message: str = None, *args, **kwargs) -> Any:
             flash("Something bad happened.")
     else:
         return output
-
-
-AnyCell = TypeVar("AnyCell", bound=Cell)
-def find_cell_by_name(cells: Sequence[AnyCell], name: str) -> AnyCell | None:
-    """Traverse through given list of cells and return first one with specified name or None, if no cell with given name found."""
-    for cell in cells:
-        if cell.name == name:
-            return cell
-    return None
